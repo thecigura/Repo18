a06cbbbbc0f72a9c888c251fa4397830c27e267c
Denan <denan.cigura@gmail.com>
2021-10-01 10:49:20 +0200

    Ex07

--- ex07/src/index.js
+++ ex07/src/index.js
@@ -1,6 +1,7 @@
 // Only change code below this line
-
+let numberArray = [9, 5, 4, 11, 5, 12, 13, 15, 4, 2, 5];
+let uniqueArray = new Set([numberArray]);
 
 // Only change code above this line
-
+console.log(uniqueArray)
 module.exports = {numberArray, uniqueArray};
